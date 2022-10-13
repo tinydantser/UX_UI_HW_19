2e1b73a58bcafed9b3790c9c887a169ce6aaa665
tinydantser <sean.dant@gmail.com>
2022-10-12 19:05:04 -0500

    feedback from instructor

--- js/index.js
+++ js/index.js
@@ -1,4 +1,78 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+let smoother = ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 0.66,
+    effects: true
+});
+
 $(document).ready(function() {
+
+    gsap.from("#fab", {
+        scrollTrigger: {
+            trigger: ".heroImg",
+            start: "bottom top",
+            end: "+=200px",
+            toggleActions: "play none none none",
+            scrub: true,
+        },
+        opacity: 0,
+        y: 200,
+        duration: 1
+    });
+
+    gsap.from("#mobileOne", {
+        scrollTrigger: {
+            trigger: "#myWork",
+            start: "top center",
+            end: "+=400",
+            toggleActions: "play none none none",
+            scrub: true,
+        },
+        opacity: 0,
+        x: "-100",
+        y: "-50",
+    });
+
+    gsap.from("#mobileTwo", {
+        scrollTrigger: {
+            trigger: "#myWork",
+            start: "top center",
+            end: "+=400",
+            toggleActions: "play none none none",
+            scrub: true
+        },
+        opacity: 0,
+        x: "100",
+        y: "-50",
+    });
+
+    gsap.from("#desktopOne", {
+        scrollTrigger: {
+            trigger: "#myWork",
+            start: "top center-=50px",
+            end: "+=600",
+            toggleActions: "play none none none",
+            scrub: true
+        },
+        opacity: 0,
+        y: 200,
+    });
+
+    gsap.from(".skillContainer", {
+        scrollTrigger: {
+            trigger: "#mySkills",
+            start: "top 80%",
+            end: "center center",
+            toggleActions: "play none none none",
+            scrub: true,
+        },
+        opacity: 0,
+        y: 100,
+        duration: 3
+    });
+
     $("button").click(function() {
         console.log("here");
     });
@@ -11,10 +85,7 @@ $(document).ready(function() {
     })
 
     $("#a_skills").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#mySkills").offset().top,
-            easing: "easin"
-        }, 500);
+        smoother.scrollTo("#mySkills", true, "center center");
     });
 
     $("#a_work").click(function() {
@@ -23,4 +94,8 @@ $(document).ready(function() {
             easing: "easin"
         }, 500);
     });
+
+    $("#a_contact").click(function() {
+        smoother.scrollTo("footer", true, "center center");
+    })
 })
